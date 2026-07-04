83b598e412c8ab415a757f0d0847a8dcd530d5e2
maurya.pg13 <mauryapg13@gmail.com>
2026-07-05 00:17:06 +0530

    Update memories database from Google Drive scan

--- js/data.js
+++ js/data.js
@@ -6,116 +6,16 @@ const MEMORIES_DATA = {
       "name": "FullSizeRender.heic"
     },
     {
-      "id": "1SjcCzdFi8QP2m-E8TR-fRPjIUD9FYRN8",
-      "name": "IMG_4114.HEIC"
-    },
-    {
       "id": "14lTTUA-yfebEHtVixomsWvkRyvyRcxT1",
       "name": "IMG_9374.HEIC"
-    },
-    {
-      "id": "1urBcDjl8BgEP5q_1dtSF8LNsESXDRwlO",
-      "name": "IMG_20250927_120046_592.jpg"
     },
     {
       "id": "1pf73b4pJW2tadJwAIK_cROwMrFpHaqND",
       "name": "IMG_20260107_143151_602.jpg"
     },
     {
-      "id": "1BfKZRiP2OCZ7veM9d9Lez7KnNdEQ75mC",
-      "name": "IMG_20260107_182610_6.jpg"
-    },
-    {
-      "id": "1qlDet20mXk8fFzke6x5vl3FpTGY6FD6_",
-      "name": "IMG_20260122_104446_914.jpg"
-    },
-    {
       "id": "1AHlqd0iHrmVO9Pvfk15pUlWVDLuYl1ue",
       "name": "IMG_20260212_073023_264.jpg"
-    },
-    {
-      "id": "1nocc591ym_XIHpbXgo2Siru5hbFyS1o-",
-      "name": "IMG_20260212_160124_980.jpg"
-    },
-    {
-      "id": "1_cJxRji5ElUTlei46k079lqkFoWb_p8z",
-      "name": "IMG_20260318_145803_974.jpg"
-    },
-    {
-      "id": "1WGT4c_y1unXZNerHlC-qIWFUbvcPM4zZ",
-      "name": "IMG-20240619-WA0005.jpg"
-    },
-    {
-      "id": "1XA8vSMqWIqjddpmgNI30QEOur7qq0Plc",
-      "name": "IMG-20240619-WA0007.jpg"
-    },
-    {
-      "id": "1eB8esjhKhzQaXc-NwfvSyBGuSTs_hjV0",
-      "name": "IMG-20240619-WA0009.jpg"
-    },
-    {
-      "id": "1XfpLAytWLT267EWdjkhqIojwU3VjxMjS",
-      "name": "IMG-20240619-WA0010.jpg"
-    },
-    {
-      "id": "1LJnUkJgEA0mjLwT9VuZJIPNQDZdHgplD",
-      "name": "IMG-20240619-WA0011.jpg"
-    },
-    {
-      "id": "1lFEAbjUUF_9gY45SzhIKnaApnXI0jWFW",
-      "name": "IMG-20240619-WA0012.jpg"
-    },
-    {
-      "id": "1yTSV40YbnyeQftrbsfcXCWE0tjDmYQoD",
-      "name": "IMG-20240619-WA0013.jpg"
-    },
-    {
-      "id": "1bB8fLvPrNeMmneDF7-2jm4WCAloR527I",
-      "name": "IMG-20240619-WA0016.jpg"
-    },
-    {
-      "id": "1mweqk0tBTHuDZAYLlYOD6eo6JmRRI4Hn",
-      "name": "IMG-20241019-WA0025.jpg"
-    },
-    {
-      "id": "1OlSH8Na4FCWOJJ-JWeIUjtiZOvs8imcp",
-      "name": "IMG-20241019-WA0026.jpg"
-    },
-    {
-      "id": "1mUkplKisHHe3RbncjttnPYJSsZLqa4xq",
-      "name": "IMG-20250401-WA0005.jpg"
-    },
-    {
-      "id": "10GjB_gPm9WK0pVjqtdsMKmzrvA3wOwYW",
-      "name": "IMG-20250503-WA0017.jpg"
-    },
-    {
-      "id": "1ROK6WC1LMKoClV6yO1YGUFzoQ3HIvbQQ",
-      "name": "IMG-20251029-WA0011.jpg"
-    },
-    {
-      "id": "1kn_-8hkbcSb6wVfX2our-HgWzWAM9z0N",
-      "name": "IMG-20251029-WA0027.jpg"
-    },
-    {
-      "id": "1Iop4ajJHO4ywQPoJ9IxU6C7JtM2vhY9Z",
-      "name": "IMG-20251029-WA0066.jpg"
-    },
-    {
-      "id": "1RB9twnHTPFAkpVykDaok8vq3SxMqDKgy",
-      "name": "IMG-20251116-WA0011.jpg"
-    },
-    {
-      "id": "1Y57tmBKCUa32w4F7qcAEUe1fW5CZst65",
-      "name": "IMG-20251129-WA0007.jpg"
-    },
-    {
-      "id": "1wVGdOUIPm7dzke_s95Xgqx2UCw71Fevw",
-      "name": "IMG-20260109-WA0003.jpg"
-    },
-    {
-      "id": "1LANWuQnPDcipCtMxiSSjkAgtPa_FpZrA",
-      "name": "IMG-20260130-WA0017.jpg"
     },
     {
       "id": "1uFo11FdFqrV1cx8E9Jh0esxfn3AB8GsI",
@@ -126,32 +26,12 @@ const MEMORIES_DATA = {
       "name": "IMG-20260212-WA0015.jpg"
     },
     {
-      "id": "1WTBKyNzkxubqiVnmBv9PHffvBvVHjZyv",
-      "name": "IMG-20260225-WA0005.jpg"
-    },
-    {
-      "id": "11w3UpfBwobAkkQPdTg7coL2LdhD2MYxT",
-      "name": "IMG-20260225-WA0006.jpg"
-    },
-    {
       "id": "1gzYsc6X6iuXovLLtH2oE-gi-ae4fcrYr",
       "name": "IMG-20260305-WA0063.jpg"
     },
     {
       "id": "1Lc0a-9TQ-IQRo0MXfC5JTd0ARvaV6r_J",
       "name": "IMG-20260311-WA0000.jpg"
-    },
-    {
-      "id": "10EH7XP0PJeghr6EXU1R5nhsn3AnQXvJX",
-      "name": "IMG-20260312-WA0058.jpg"
-    },
-    {
-      "id": "1k802bpfroISknWwzfdK5VlOkTigLjDkv",
-      "name": "IMG-20260313-WA0006.jpg"
-    },
-    {
-      "id": "12VKp6bR_QZR8k-CDJwwuY-8x70VUPlpz",
-      "name": "IMG-20260318-WA0092.jpg"
     },
     {
       "id": "1G-PQRNkm7kPoXliMpfvkRFBjDVij0zGC",
@@ -166,12 +46,12 @@ const MEMORIES_DATA = {
       "name": "IMG-20260411-WA0055.jpg"
     },
     {
-      "id": "11DepZ7_cp2hAaNC7o7J9tncLRCHfxB1S",
-      "name": "IMG-20260411-WA0163.jpg"
+      "id": "19lW3zC7U8xpy8qwjpaoIUdjr0qUhefuR",
+      "name": "IMG-20260420-WA0019.jpg"
     },
     {
-      "id": "11DepZ7_cp2hAaNC7o7J9tncLRCHfxB1S",
-      "name": "IMG-20260411-WA0163.jpg"
+      "id": "1EKWXkqHOT_PdM8exr1KcsLN4cdLfoEAX",
+      "name": "IMG-20260702-WA0030.jpg"
     }
   ],
   "Alvin": [
@@ -192,28 +72,12 @@ const MEMORIES_DATA = {
       "name": "IMG-20260411-WA0393.jpg"
     },
     {
-      "id": "1JmYKcUbg7fOQK8DqeR6_Puc_0CdX4y3g",
-      "name": "IMG-20260411-WA0394.jpg"
-    },
-    {
-      "id": "1Hr_C9DsH7r8zn9xo15WxnQYgn9id0PME",
-      "name": "IMG-20260411-WA0395.jpg"
-    },
-    {
       "id": "1PR6N84IBtS-IqtFbDORJYSx6VWzQ8zQd",
       "name": "IMG-20260411-WA0420.jpg"
     },
     {
       "id": "1DgAi03jqsPiKZ_LjasruulcMlk8-WZpx",
       "name": "IMG-20260622-WA0005.jpg"
-    },
-    {
-      "id": "1_AZ4eqdiKLQKTKBrEDIq5n8Bjnhc_abC",
-      "name": "WhatsApp Image 2026-06-28 at 20.10.58 (1).jpeg"
-    },
-    {
-      "id": "1NPeL23TtKw_PDcygMOzL6xIJAfELh3Zv",
-      "name": "WhatsApp Image 2026-06-28 at 20.10.58.jpeg"
     },
     {
       "id": "1dsKyWznczTscPlr4hmXP-pw-5dPDLTqr",
@@ -226,14 +90,6 @@ const MEMORIES_DATA = {
       "name": "7150e498-9baf-4b1f-b7ba-a7a560b356e3.jpg"
     },
     {
-      "id": "1EGR2QJ8kl-65RJwyjov9z78p_ZvlNZFz",
-      "name": "1000033136.jpg"
-    },
-    {
-      "id": "1rR7wMkPf5cY3zQbWJ1oo_0DpUJbRZIXL",
-      "name": "Copy of IMG-20251029-WA0013.jpg"
-    },
-    {
       "id": "1xIqkkuXZTdZVp_TTTi1cmMiIT_LO-9JB",
       "name": "IMG_20260122_104456_294.jpg"
     },
@@ -244,10 +100,6 @@ const MEMORIES_DATA = {
     {
       "id": "18AhforZtxPQN-E7kmitNsuS5qtqmDoTO",
       "name": "IMG-20251029-WA0015.jpg"
-    },
-    {
-      "id": "10flhAnQEywVT-r_BK4y7AtS3oME5kxky",
-      "name": "IMG-20251029-WA0019.jpg"
     },
     {
       "id": "1fjKiGPiJoP2EYF-TdaYi8AbMuB2d1U-G",
@@ -282,28 +134,12 @@ const MEMORIES_DATA = {
       "name": "IMG-20260411-WA0338.jpg"
     },
     {
-      "id": "1aC9WEds8dE82WKY7xhMSG11SQGdY4MkW",
-      "name": "IMG-20260411-WA0362.jpg"
-    },
-    {
-      "id": "1-NBv7agg1WfBblnmSlvns0lMq9p85iXK",
-      "name": "IMG-20260411-WA0421.jpg"
-    },
-    {
       "id": "1bWvZ0z-c7lnRbJ_BTanhXcYgeKBggLS2",
       "name": "IMG-20260411-WA0550.jpg"
     },
     {
-      "id": "12F78upzJQBi_VFJyB7MSPVFTGEYqLmxi",
-      "name": "IMG-20260502-WA0014.jpg"
-    },
-    {
       "id": "1pDb14AzgpK7MaCbDlw2fpTNnit6jLBU5",
       "name": "IMG-20260502-WA0016.jpg"
-    },
-    {
-      "id": "1KmRs-7msn4L2vhW2VJY337xkHp7zxk9P",
-      "name": "IMG-20260511-WA0059.jpg"
     },
     {
       "id": "1MkVmB094pDTpiL3jWEZB9t3cttuketpb",
@@ -316,24 +152,12 @@ const MEMORIES_DATA = {
       "name": "IMG-20260408-WA0007.jpg"
     },
     {
-      "id": "12Txkki_AzHP6hHRcFqaLY6cTqZ1nveDN",
-      "name": "IMG-20260411-WA0451.jpg"
-    },
-    {
-      "id": "1DFZ_aDctLSh2BlBlyTkOSOeorA6Q3z91",
-      "name": "IMG-20260411-WA0461.jpg"
+      "id": "132FPk1fNuG9nXr5VPQX9r5Gw2aROTCzy",
+      "name": "IMG-20260411-WA0424.jpg"
     },
     {
       "id": "1Jfb492DzB4wIiOuGaIfTslHSUtSvrWh2",
       "name": "IMG-20260411-WA0473.jpg"
-    },
-    {
-      "id": "1nzyeKMJAt-_bhrp38n4XccL5Vqjb8qQQ",
-      "name": "IMG-20260411-WA0474.jpg"
-    },
-    {
-      "id": "1RhrNOt2qpRQZaVyy71KPfTU6wALHCqcy",
-      "name": "IMG-20260411-WA0476.jpg"
     }
   ],
   "grp photos": [
@@ -350,20 +174,132 @@ const MEMORIES_DATA = {
       "name": "IMG_20250828_144800_745.jpg"
     },
     {
-      "id": "1dDN2wSmixqvhRSu7M616FRyKmv1F4Da3",
-      "name": "IMG_20250828_144848_942.jpg"
-    },
-    {
-      "id": "16_Lt0L7Te0i7ZGQTodUEmrMrmNXhNsvU",
-      "name": "IMG_20250828_145547_922.jpg"
-    },
-    {
       "id": "10bTvXcjs23pLcg-_x8vQMPxz6EaRDbuv",
       "name": "IMG_20250828_155408_436.jpg"
     },
     {
       "id": "1vjxYFeaRq_lpwrBQbCo_ksyKxU3ujhCG",
       "name": "IMG_20250927_120013_676.jpg"
+    },
+    {
+      "id": "1BfKZRiP2OCZ7veM9d9Lez7KnNdEQ75mC",
+      "name": "IMG_20260107_182610_6.jpg"
+    },
+    {
+      "id": "1_cJxRji5ElUTlei46k079lqkFoWb_p8z",
+      "name": "IMG_20260318_145803_974.jpg"
+    },
+    {
+      "id": "1bB8fLvPrNeMmneDF7-2jm4WCAloR527I",
+      "name": "IMG-20240619-WA0016.jpg"
+    },
+    {
+      "id": "1mweqk0tBTHuDZAYLlYOD6eo6JmRRI4Hn",
+      "name": "IMG-20241019-WA0025.jpg"
+    },
+    {
+      "id": "1OlSH8Na4FCWOJJ-JWeIUjtiZOvs8imcp",
+      "name": "IMG-20241019-WA0026.jpg"
+    },
+    {
+      "id": "1mUkplKisHHe3RbncjttnPYJSsZLqa4xq",
+      "name": "IMG-20250401-WA0005.jpg"
+    },
+    {
+      "id": "1ROK6WC1LMKoClV6yO1YGUFzoQ3HIvbQQ",
+      "name": "IMG-20251029-WA0011.jpg"
+    },
+    {
+      "id": "1Iop4ajJHO4ywQPoJ9IxU6C7JtM2vhY9Z",
+      "name": "IMG-20251029-WA0066.jpg"
+    },
+    {
+      "id": "1RB9twnHTPFAkpVykDaok8vq3SxMqDKgy",
+      "name": "IMG-20251116-WA0011.jpg"
+    },
+    {
+      "id": "1Y57tmBKCUa32w4F7qcAEUe1fW5CZst65",
+      "name": "IMG-20251129-WA0007.jpg"
+    },
+    {
+      "id": "1k802bpfroISknWwzfdK5VlOkTigLjDkv",
+      "name": "IMG-20260313-WA0006.jpg"
+    },
+    {
+      "id": "12VKp6bR_QZR8k-CDJwwuY-8x70VUPlpz",
+      "name": "IMG-20260318-WA0092.jpg"
+    },
+    {
+      "id": "1Fw-Ti85DH9C8zUaF2AAdaJ43HQ_oU16S",
+      "name": "IMG-20260411-WA0279.jpg"
+    },
+    {
+      "id": "1gsYBo9aRHr9_-S5edzPBDyWhWCD9Sj7u",
+      "name": "IMG-20260411-WA0329.jpg"
+    },
+    {
+      "id": "1zjS_Fzv2SZEuW4OlGBKBhuwGvLiHiXSQ",
+      "name": "IMG-20260411-WA0345.jpg"
+    },
+    {
+      "id": "1gu7RQ3_nue7IYBuXK4Flc1tpUZ2Fj825",
+      "name": "IMG-20260411-WA0416.jpg"
+    },
+    {
+      "id": "1W-6ktuorwOqOjLoUTpsMAPv-Tq9NAC-3",
+      "name": "IMG-20260411-WA0428.jpg"
+    },
+    {
+      "id": "1_RHaAiH6GTUr8irxk4Q-nHTZkkcg5mtb",
+      "name": "IMG-20260420-WA0001.jpg"
+    },
+    {
+      "id": "1sqIhEewRtQmhNiNuc8dQXSJPK6SMS7La",
+      "name": "IMG-20260425-WA0041.jpg"
+    },
+    {
+      "id": "1XFW3utXDHmHeSRfFz7appP7d5aK3cDx2",
+      "name": "IMG-20260511-WA0019.jpg"
+    },
+    {
+      "id": "1eClR6VtaQJWoQvL8FBvRbHkr3t-oLw0X",
+      "name": "IMG-20260511-WA0023.jpg"
+    },
+    {
+      "id": "1lclgP0-9Opyxe9WsNywAM5M-HF1UrJOA",
+      "name": "IMG-20260511-WA0040.jpg"
+    },
+    {
+      "id": "13R7TgjMOQmjbY06mo8TdN4A6SyxigbFy",
+      "name": "IMG-20260511-WA0051.jpg"
+    },
+    {
+      "id": "139_QF9ldk3ZzoISOh6hmFUDUlr3ILHcD",
+      "name": "VID_20260107_182604.mp4"
+    },
+    {
+      "id": "1OuegFAHlGx6grkmIqI9zA7gxd1XcW9Ro",
+      "name": "WhatsApp Image 2026-06-28 at 20.17.10 (1).jpeg"
+    },
+    {
+      "id": "1dLt8nKi-oQmNKU7IFYf78YWTkLAqbRyg",
+      "name": "WhatsApp Image 2026-06-28 at 20.17.10 (2).jpeg"
+    },
+    {
+      "id": "12aRJPQ6A_O3yuHhhGtaCj8yweHUw3MoU",
+      "name": "WhatsApp Image 2026-06-28 at 20.17.11 (1).jpeg"
+    },
+    {
+      "id": "1BcOl7r1ImxBe7_uEEnPSHAcTnOTZw4rn",
+      "name": "WhatsApp Image 2026-06-28 at 20.17.12 (1).jpeg"
+    },
+    {
+      "id": "1N6mceYPuSwVYFDTSKLoCe0mYWrFbVc5e",
+      "name": "WhatsApp Image 2026-06-28 at 20.17.12 (2).jpeg"
+    },
+    {
+      "id": "1jkgbdyVN0xKVdGHJTAkIKbMDOvghUK_0",
+      "name": "WhatsApp Image 2026-06-28 at 20.17.12.jpeg"
     }
   ],
   "Kruti": [
@@ -398,9 +334,17 @@ const MEMORIES_DATA = {
     {
       "id": "1O9xz_1m2Ns3BGWwCF6ZjZdplxjG8S5K5",
       "name": "IMG_20260331_154317_737.jpg"
+    },
+    {
+      "id": "1lFEAbjUUF_9gY45SzhIKnaApnXI0jWFW",
+      "name": "IMG-20240619-WA0012.jpg"
     }
   ],
   "Maurya": [
+    {
+      "id": "1LANWuQnPDcipCtMxiSSjkAgtPa_FpZrA",
+      "name": "IMG-20260130-WA0017.jpg"
+    },
     {
       "id": "16K3n1q5rPrlZWk9nTxzjr7Mi7ug19q48",
       "name": "IMG-20260704-WA0001.jpg"
@@ -420,6 +364,18 @@ const MEMORIES_DATA = {
   ],
   "Pranav": [
     {
+      "id": "1kn_-8hkbcSb6wVfX2our-HgWzWAM9z0N",
+      "name": "IMG-20251029-WA0027.jpg"
+    },
+    {
+      "id": "1wVGdOUIPm7dzke_s95Xgqx2UCw71Fevw",
+      "name": "IMG-20260109-WA0003.jpg"
+    },
+    {
+      "id": "1MIgIqHmuCeU4xW1aNEnT9Qzs3ql0HKey",
+      "name": "IMG-20260411-WA0307.jpg"
+    },
+    {
       "id": "1usxLGxGm00KeBPjxGHm50z4vJ5gzjiZa",
       "name": "IMG-20260411-WA0308.jpg"
     },
@@ -430,9 +386,17 @@ const MEMORIES_DATA = {
     {
       "id": "1pZrhY-tjbNrCpG0flE8XGWBKzzOZcQH8",
       "name": "IMG-20260411-WA0310.jpg"
+    },
+    {
+      "id": "1KmRs-7msn4L2vhW2VJY337xkHp7zxk9P",
+      "name": "IMG-20260511-WA0059.jpg"
     }
   ],
   "Taran": [
+    {
+      "id": "1SjcCzdFi8QP2m-E8TR-fRPjIUD9FYRN8",
+      "name": "IMG_4114.HEIC"
+    },
     {
       "id": "1br00orYUD3yvEJhbjQ7HoybuWdVGgNl8",
       "name": "IMG-20250915-WA0008.jpg"
